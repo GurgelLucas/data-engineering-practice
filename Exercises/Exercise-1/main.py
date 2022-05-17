94f9fbf3517e881724a2b8a3c24fbe24028afad5
GurgelLucas <lgurgel13@gmail.com>
2022-05-17 18:51:56 -0300

    [X] Exercise 01 - Resolved

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from zipfile import ZipFile
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -13,8 +15,34 @@ download_uris = [
 
 def main():
     # your code here
-    pass
+    try:
+        os.mkdir('./downloads')
+        print("Diretório 'downloads' foi criado")
+    except OSError:
+        print("Diretório já existente")
+        
+    for i in download_uris:
+        # Captura o nome dos arquivos
+        file_name = [name for name in i.split("/")]
+        
+        try:
+            # Realiza a requisição
+            res = requests.get(i)
+            if (res.status_code == 200):
+                # Etapa de Salvar o arquivo no diretorio criado anteriormente
+                with open(f'./downloads/{file_name[-1]}', 'wb') as file:
+                    file.write(res.content) 
 
+                # Etapa de extrair os arquivos Zip que foram salvos
+                with ZipFile(f'./downloads/{file_name[-1]}', 'r') as fileZip:
+                    fileZip.extractall("downloads")
+
+                # Etapa de remover os arquivos zip 
+                os.remove(f'./downloads/{file_name[-1]}')
+        
+        # Caso ocorra uma excessão na requisição.
+        except requests.RequestException:
+            print("Erro no download do arquivo")
 
 if __name__ == '__main__':
     main()
